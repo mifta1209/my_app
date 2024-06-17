ad536ecc84250530de5024c329f066d2de7dabcd
mifta1209 <miftahokta.2021@student.uny.ac.id>
2024-06-17 14:53:29 +0000

    membuat users, article dan comment pada migrate di db

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :articles
+  resources :users
   get 'home/index'
   get 'read/:article_id', to: 'home#read', as: "read"
   post 'save-comment/:article_id', to: 'home#save_comment', as: "save_comment"
